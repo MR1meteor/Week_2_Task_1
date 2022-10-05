9e854caa4bbd427b633248e72dae0243567fa450
Alexey Safronov <alexey-safronov@inbox.ru>
2022-10-05 21:30:24 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "Сейчас", "будут", "происходить", "абсолютно", "непонятные", "вещи." };
+            bool boolVar;
+
+            try
+            {
+                boolVar = Convert.ToBoolean(array);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Oops, Ваш код - дерьмо: {e.Message}");
+            }
+        }
+    }
+}
